749e157aa2c228bff424db02a83f598c773e96a4
Hhbhari <159613769+Hhbhari@users.noreply.github.com>
2024-03-01 22:39:15 +0530

    add stack logic on script

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const value = document.querySelector('.display');
-let start=0;
-let var1=0;
-let prevOp =0;
+let count=0, dotCount=0, negCount=0;
+
+class Stack{
+    constructor(){
+        this.items=[];
+    }
+
+    add(ele){
+        this.items.push(ele);
+    }
+
+    remove(){
+        if(this.items.length>0){
+            return this.items.pop();
+        }
+    }
+
+    isEmpty(){
+        return this.items.length==0;
+    }
+
+    peek(){
+        return this.items[this.items.length-1];
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+    clear(){
+        this.items=[];
+    }
+}
+
+let stack = new Stack();
+
 
 const buttonsAll = document.querySelectorAll('button')
 
@@ -11,6 +44,7 @@ buttonsAll.forEach((buttons)=>{
     });
 })
 
-function calculation(selection){
-    console.log('check');
-}
+
+
+
+
